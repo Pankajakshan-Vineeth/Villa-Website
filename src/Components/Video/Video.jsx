f757cb72c0e4e8752b7300d4bb5cdb86f6882cf2
Pankajakshan Vineeth <yamunapankaj99@gmail.com>
2025-06-21 22:01:23 +0530

    video component wip

--- src/Components/Video/Video.jsx
+++ src/Components/Video/Video.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Video.css'
 import frame from '../../assets/video-frame.jpg'
 import play from '../../assets/play.png'
+import orangedot from '../../assets/orange.png'
 
 const Video = () => {
   return (
@@ -13,6 +14,29 @@ const Video = () => {
       <div className='second-image'>
         <img src={frame}alt="" className='frame-image' />
         <img src={play}alt="" className='play-icon'/>
+      </div>
+      <div className='orange-dots'> 
+      <div className='dot-container'>
+       <img src={orangedot} alt="" className='orange-icon' />
+       <div className="orange-text">
+        <h1>34</h1>
+        <p>Buildings Finished Now</p>
+       </div>
+       </div>
+       <div className='dot-container'>
+       <img src={orangedot} alt="" className='orange-icon'/>
+       <div className="orange-text">
+        <h1>34</h1>
+        <p>Buildings Finished Now</p>
+       </div>
+       </div>
+       <div className='dot-container'>
+       <img src={orangedot} alt="" className='orange-icon'/>
+       <div className="orange-text">
+        <h1>34</h1>
+        <p>Buildings Finished Now</p>
+       </div>
+       </div>
       </div>
     </div>
   )
