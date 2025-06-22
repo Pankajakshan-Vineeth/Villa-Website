22726abaf9e60e0bbdb764617a9b85db0270a21e
Pankajakshan Vineeth <yamunapankaj99@gmail.com>
2025-06-22 13:22:32 +0530

    video component completed

--- src/Components/Video/Video.jsx
+++ src/Components/Video/Video.jsx
@@ -1,45 +1,54 @@
-import React from 'react'
-import './Video.css'
-import frame from '../../assets/video-frame.jpg'
-import play from '../../assets/play.png'
-import orangedot from '../../assets/orange.png'
+import React from "react";
+import "./Video.css";
+import frame from "../../assets/video-frame.jpg";
+import play from "../../assets/play.png";
+import orangedot from "../../assets/orange.png";
 
 const Video = () => {
   return (
-    <div className='video-container'>
-      <div className='video-text'>
+    <div className="video-container">
+      <div className="video-text">
         <p>| VIDEO VIEW</p>
-        <h1 className='h1-text'>Get Closer View & Different Feeling</h1>
+        <h1 className="h1-text">Get Closer View & Different Feeling</h1>
       </div>
-      <div className='second-image'>
-        <img src={frame}alt="" className='frame-image' />
-        <img src={play}alt="" className='play-icon'/>
+      <div className="second-image">
+        <img src={frame} alt="" className="frame-image" />
+        <img src={play} alt="" className="play-icon" />
       </div>
-      <div className='orange-dots'> 
-      <div className='dot-container'>
-       <img src={orangedot} alt="" className='orange-icon' />
-       <div className="orange-text">
-        <h1>34</h1>
-        <p>Buildings Finished Now</p>
-       </div>
-       </div>
-       <div className='dot-container'>
-       <img src={orangedot} alt="" className='orange-icon'/>
-       <div className="orange-text">
-        <h1>34</h1>
-        <p>Buildings Finished Now</p>
-       </div>
-       </div>
-       <div className='dot-container'>
-       <img src={orangedot} alt="" className='orange-icon'/>
-       <div className="orange-text">
-        <h1>34</h1>
-        <p>Buildings Finished Now</p>
-       </div>
-       </div>
+      <div className="orange-dots">
+        <div className="dot-container">
+          <img src={orangedot} alt="" className="orange-icon" />
+          <div className="orange-text">
+            <h1>34</h1>
+            <p>
+              Buildings <br />
+              Finished Now
+            </p>
+          </div>
+        </div>
+        <div className="dot-container">
+          <img src={orangedot} alt="" className="orange-icon" />
+          <div className="orange-text">
+            <h1>34</h1>
+            <p>
+              {" "}
+              Years <br /> Experience
+            </p>
+          </div>
+        </div>
+        <div className="dot-container">
+          <img src={orangedot} alt="" className="orange-icon" />
+          <div className="orange-text">
+            <h1>34</h1>
+            <p>
+              Awwards <br />
+              Won 2023
+            </p>
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Video
+export default Video;
