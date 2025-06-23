492a231914e88d017282939e6b52335917edc3e6
Pankajakshan Vineeth <yamunapankaj99@gmail.com>
2025-06-23 12:20:40 +0530

    property component completed

--- src/Components/Properties/Properties.jsx
+++ src/Components/Properties/Properties.jsx
@@ -4,55 +4,176 @@ import property03 from "../../assets/property-03.jpg";
 import property04 from "../../assets/property-04.jpg";
 import property05 from "../../assets/property-05.jpg";
 import property06 from "../../assets/property-06.jpg";
+import "./Properties.css";
 const Properties = () => {
   return (
     <div className="property-container">
       <div className="property-text">
         <h3>| PROPERTIES</h3>
-        <h1>We Provide The Best Property You Like</h1>
+        <h1>
+          We Provide The Best <br />
+          Property You Like
+        </h1>
       </div>
       <div className="card-container">
-        <div className="card">
-          <img src={property01} alt="" />
+        <div className="cards">
+          <img src={property01} alt="" className="property-image" />
+
           <div class="property-header">
-            <span class="badge">Apartment</span>
-            <span class="price">$584.500</span>
+            <span class="badge">Luxury Villa</span>
+            <span class="price">$2.264.000</span>
           </div>
 
-          <h2 class="address">12 New Street Miami, OR 12650</h2>
+          <h3 class="address">12 New Street Miami, OR 12650</h3>
+
           <div class="info">
             <p>
-              <strong>Bedrooms:</strong> 4
+              <strong>Bedrooms:</strong> 4{" "}
+              <span className="bathroom">
+                <strong>Bathroom</strong> :8
+              </span>
             </p>
             <p>
-              <strong>Bathrooms:</strong> 3
-            </p>
-            <p>
-              <strong>Area:</strong> <span class="highlight">125m2</span>
-            </p>
-            <p>
-              <strong>Floor:</strong> 25th
-            </p>
-            <p>
-              <strong>Parking:</strong> <span class="highlight">2 cars</span>
+              <strong>Area:</strong>{" "}
+              <span class="highlight">
+                125m2 <strong className="bathroom">Floor:</strong> 25th{" "}
+                <strong className="bathroom">Parking:</strong>{" "}
+                <span class="highlight">2 cars</span>
+              </span>
             </p>
           </div>
+
           <button class="visit-btn">Schedule a visit</button>
         </div>
-        <div className="card">
-          <img src={property02} alt="" />
+        <div className="cards">
+          <img src={property02} alt="" className="property-image" />
+          <div class="property-header">
+            <span class="badge">Luxury Villa</span>
+            <span class="price">$2.264.000</span>
+          </div>
+          <h3 class="address">12 New Street Miami, OR 12650</h3>
+          <div class="info">
+            <p>
+              <strong>Bedrooms:</strong> 4{" "}
+              <span className="bathroom">
+                <strong>Bathroom</strong> :8
+              </span>
+            </p>
+            <p>
+              <strong>Area:</strong>{" "}
+              <span class="highlight">
+                125m2 <strong className="bathroom">Floor:</strong> 25th{" "}
+                <strong className="bathroom">Parking:</strong>{" "}
+                <span class="highlight">2 cars</span>
+              </span>
+            </p>
+          </div>
+
+          <button class="visit-btn">Schedule a visit</button>
         </div>
-        <div className="card">
-          <img src={property03} alt="" />
+        <div className="cards">
+          <img src={property03} alt="" className="property-image" />
+          <div class="property-header">
+            <span class="badge">Luxury Villa</span>
+            <span class="price">$2.264.000</span>
+          </div>
+          <h3 class="address">12 New Street Miami, OR 12650</h3>
+          <div class="info">
+            <p>
+              <strong>Bedrooms:</strong> 4{" "}
+              <span className="bathroom">
+                <strong>Bathroom</strong> :8
+              </span>
+            </p>
+            <p>
+              <strong>Area:</strong>{" "}
+              <span class="highlight">
+                125m2 <strong className="bathroom">Floor:</strong> 25th{" "}
+                <strong className="bathroom">Parking:</strong>{" "}
+                <span class="highlight">2 cars</span>
+              </span>
+            </p>
+          </div>
+
+          <button class="visit-btn">Schedule a visit</button>
         </div>
-        <div className="card">
-          <img src={property04} alt="" />
+        <div className="cards">
+          <img src={property04} alt="" className="property-image" />
+          <div class="property-header">
+            <span class="badge">Luxury Villa</span>
+            <span class="price">$2.264.000</span>
+          </div>
+          <h3 class="address">12 New Street Miami, OR 12650</h3>
+          <div class="info">
+            <p>
+              <strong>Bedrooms:</strong> 4{" "}
+              <span className="bathroom">
+                <strong>Bathroom</strong> :8
+              </span>
+            </p>
+            <p>
+              <strong>Area:</strong>{" "}
+              <span class="highlight">
+                125m2 <strong className="bathroom">Floor:</strong> 25th{" "}
+                <strong className="bathroom">Parking:</strong>{" "}
+                <span class="highlight">2 cars</span>
+              </span>
+            </p>
+          </div>
+
+          <button class="visit-btn">Schedule a visit</button>
         </div>
-        <div className="card">
-          <img src={property05} alt="" />
+        <div className="cards">
+          <img src={property05} alt="" className="property-image" />
+          <div class="property-header">
+            <span class="badge">Luxury Villa</span>
+            <span class="price">$2.264.000</span>
+          </div>
+          <h3 class="address">12 New Street Miami, OR 12650</h3>
+          <div class="info">
+            <p>
+              <strong>Bedrooms:</strong> 4{" "}
+              <span className="bathroom">
+                <strong>Bathroom</strong> :8
+              </span>
+            </p>
+            <p>
+              <strong>Area:</strong>{" "}
+              <span class="highlight">
+                125m2 <strong className="bathroom">Floor:</strong> 25th{" "}
+                <strong className="bathroom">Parking:</strong>{" "}
+                <span class="highlight">2 cars</span>
+              </span>
+            </p>
+          </div>
+
+          <button class="visit-btn">Schedule a visit</button>
         </div>
-        <div className="card">
-          <img src={property05} alt="" />
+        <div className="cards">
+          <img src={property06} alt="" className="property-image" />
+          <div class="property-header">
+            <span class="badge">Luxury Villa</span>
+            <span class="price">$2.264.000</span>
+          </div>
+          <h3 class="address">12 New Street Miami, OR 12650</h3>
+          <div class="info">
+            <p>
+              <strong>Bedrooms:</strong> 4{" "}
+              <span className="bathroom">
+                <strong>Bathroom</strong> :8
+              </span>
+            </p>
+            <p>
+              <strong>Area:</strong>{" "}
+              <span class="highlight">
+                125m2 <strong className="bathroom">Floor:</strong> 25th{" "}
+                <strong className="bathroom">Parking:</strong>{" "}
+                <span class="highlight">2 cars</span>
+              </span>
+            </p>
+          </div>
+
+          <button class="visit-btn">Schedule a visit</button>
         </div>
       </div>
     </div>
